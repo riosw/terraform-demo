880735496c48da2fde2c51bdd7452e32e3896129
Ong Rio <ongrios.wijaya@gmail.com>
2022-05-21 19:57:46 +0700

    Change instance naming to "terraform-demo"

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ provider "aws" {
   region = "ap-southeast-3"
 }
 
-resource "aws_instance" "server-example" {
+resource "aws_instance" "terraform-demo" {
     ami = "ami-0c1460efd8855de7c"
     instance_type = "t3.micro"
+
+    tags = {
+      Name = "terraform-demo"
+    }
 }
